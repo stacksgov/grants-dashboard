fc75dd102116bd217f0f1c6e82ae341a3f3f9801
Vidia <komacine@protonmail.com>
2022-06-21 17:00:11 +0100

    do landing page

--- pages/application.js
+++ pages/application.js
@@ -1,28 +1,28 @@
-import styles from './Application.module.css';
-import Nav from '../components/application/layout/Nav';
-import Steps from '../components/application/layout/Steps';
-import StacksLogo from '../public/images/stacks-logo.svg';
-import OkButton from '../components/application/layout/OkButton';
-import Wishlist from '../components/application/Wishlist';
+import styles from "./Application.module.css";
+import Nav from "../components/application/layout/Nav";
+import Steps from "../components/application/layout/Steps";
+import StacksLogo from "../public/images/stacks-logo.svg";
+import OkButton from "../components/application/layout/OkButton";
+import Wishlist from "../components/application/Wishlist";
 
 const Application = () => {
-	return (
-		<div className={styles.applicationWrapper}>
-			<Nav />
-			<div className={styles.mainComponents}>
-				<div>
-					<Steps />
-				</div>
-				<div>
-					<Wishlist />
-				</div>
-				<div>
-					<OkButton />
-				</div>
-			</div>
-			<StacksLogo className={styles.stacksSVG} />
-		</div>
-	);
+  return (
+    <div className={styles.applicationWrapper}>
+      <Nav />
+      <div className={styles.mainComponents}>
+        <div>
+          <Steps />
+        </div>
+        <div>
+          <Wishlist />
+        </div>
+        <div>
+          <OkButton />
+        </div>
+      </div>
+      <StacksLogo className={styles.stacksSVG} />
+    </div>
+  );
 };
 
 export default Application;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,55 @@
-import { useSession, signIn, signOut } from 'next-auth/react';
+import { useSession, signIn, signOut } from "next-auth/react";
+import Blob from "../public/images/blob.svg";
+import styles from "./Index.module.css";
+import { AiFillGithub } from "react-icons/ai";
 
 const Home = () => {
-	const { data: session } = useSession();
+  const { data: session } = useSession();
 
-	return <div>Hello</div>;
+  return (
+    <div className={styles.indexBackground}>
+      <div className={styles.indexWrapper}>
+        <div className={styles.leftWrapper}>
+          <p>Stacks Foundation</p>
+          <h1>Project Launchpad</h1>
+          <p>
+            Committed to helping developers, designers, community leaders,
+            artists, entrepreneurs, and more build a more...
+          </p>
+          <p>
+            User-owned & open-source internet powered by the Stacks blockchain,
+            Clarity smart contracts, and secured by Bitcoin.
+          </p>
+        </div>
+        <div className={styles.rightWrapper}>
+          <Blob className={styles.blob} />
+
+          <div className={styles.funding}>
+            <p>Funding available from:</p>
+            <p>One-thousand to two-hundred and fifty thuosand dollars.</p>
+            <button>Submit Your Application</button>
+            <p>GitHub Account Required</p>
+          </div>
+          <div className={styles.basics}>
+            <p>Want to learn the basics?</p>
+            <p>Check out our educational Project Launchpad Primer.</p>
+            <button>Project Launchpad Primer</button>
+          </div>
+
+          <div className={styles.basics}>
+            <p>Want to explore the details?</p>
+            <p>Vist the Project Launchpad GitHub Repository.</p>
+            <button>
+              <div> Project Launchpad Repository</div>
+              <div>
+                <AiFillGithub size="25" />
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
