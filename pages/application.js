0059cea72a2f64ecbdf1cbd3f37d46c6fd4d9e0f
Diopitis <54492027+diopitis@users.noreply.github.com>
2022-06-28 14:48:53 -0500

    added switch statement to conditionally render components

--- pages/application.js
+++ pages/application.js
@@ -4,14 +4,14 @@ import Steps from '../components/application/layout/Steps';
 import StacksLogo from '../public/images/stacks-logo.svg';
 import UseOkButton from '../components/application/layout/OkButton';
 import Wishlist from '../components/application/Wishlist';
-// import FundingStream from '../components/application/FundingStream';
-// import ProjectTrack from '../components/application/ProjectTrack';
-// import ProjectTags from '../components/application/ProjectTags';
-// import ProjectInformation from '../components/application/ProjectInformation';
-// import ProjectRoadmap from '../components/application/ProjectRoadmap';
-// import MissionStatement from '../components/application/MissionStatement';
-// import ProjectLinks from '../components/application/ProjectLinks';
-// import Success from '../components/application/Success';
+import FundingStream from '../components/application/FundingStream';
+import ProjectTrack from '../components/application/ProjectTrack';
+import ProjectTags from '../components/application/ProjectTags';
+import ProjectInformation from '../components/application/ProjectInformation';
+import ProjectRoadmap from '../components/application/ProjectRoadmap';
+import MissionStatement from '../components/application/MissionStatement';
+import ProjectLinks from '../components/application/ProjectLinks';
+import Success from '../components/application/Success';
 
 const Application = () => {
 	const { OkButton, currentStep } = UseOkButton();
@@ -23,15 +23,37 @@ const Application = () => {
 					<Steps step={currentStep} />
 				</div>
 				<div>
-					<Wishlist />
-					{/* <FundingStream /> */}
-					{/* <ProjectTrack /> */}
-					{/* <ProjectTags /> */}
-					{/* <ProjectInformation /> */}
-					{/* <ProjectRoadmap /> */}
-					{/* <MissionStatement /> */}
-					{/* <ProjectLinks /> */}
-					{/* <Success /> */}
+					{() => {
+						switch (currentStep) {
+							case 1:
+								return <Wishlist />;
+							case 2:
+								return <FundingStream />;
+							case 3: {
+								return <ProjectTrack />;
+							}
+							case 4: {
+								return <ProjectTags />;
+							}
+							case 4: {
+								return <ProjectInformation />;
+							}
+							case 4: {
+								return <ProjectRoadmap />;
+							}
+							case 5: {
+								return <MissionStatement />;
+							}
+							case 6: {
+								return <ProjectLinks />;
+							}
+							case 7: {
+								return <Success />;
+							}
+							default:
+								return <FundingStream />;
+						}
+					}}
 				</div>
 				<div>{OkButton}</div>
 			</div>
