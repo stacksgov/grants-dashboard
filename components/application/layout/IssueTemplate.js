daf4cd90ee0576b8b40e8ad7d700b8fa68af72c1
kj <komacine@protonmail.com>
2022-07-01 14:28:17 +0100

    add markdown

--- components/application/layout/IssueTemplate.js
+++ components/application/layout/IssueTemplate.js
@@ -1,43 +1,47 @@
 const IssueTemplate = (issue) => {
-	console.log('ISSUE', issue);
-	console.log('selected values ', selectedValues);
+  console.log("ISSUE", issue);
+  console.log("selected values ", selectedValues);
 
-	// let selectedValues = issue.filter((val) => console.log('VALL', val));
+  // let selectedValues = issue.filter((val) => console.log('VALL', val));
 
-	let selectedValues = Object.keys(issue).map((key) => console.log(issue[key].length));
+  let selectedValues = Object.keys(issue).map((key) => {
+    console.log("CHOICE:", issue[key]);
+    return issue[key]?.choice;
+  });
 
-	console.log('selected values before: ', selectedValues);
+  console.log("selected values before: ", selectedValues);
 
-	selectedValues = selectedValues.filter((e) => e);
+  selectedValues = selectedValues.filter((e) => e);
 
-	console.log('selected values after: ', selectedValues);
+  console.log("selected values after: ", selectedValues);
+  console.log("ISSSUEEEEEE ", issue);
 
-	let template = `
-    
-    # PROJECT BASICS
+  let template = `# PROJECT BASICS
+  **Project Name:** ${issue.name}
+  **Project Budget:** ${issue.budget}
+  **Project Duration:** ${issue.duration}
+  **Funding Stream:** ${selectedValues[0]}
+  **Project Type:** ${selectedValues[1]}
+  **Project Track:** ${selectedValues[2]}
+  **Project Goal:** ${selectedValues[3]}
+  **Project Audience:** ${selectedValues[4]}
+  **Project Openness:** ${selectedValues[5]}
+  **Project Team Members:** ${issue.team}
+  
+  # PROJECT ROADMAP & DELIVERABLES
+  **Final Deliverable:** ${issue.final}
+  **Milestone Deliverable 1:** ${issue.milestoneOne}
+  
+  # PROJECT MISSION & LINKS
+  
+  **[${issue.firstLink}](url):** ${issue.firstLinkDescription}
+  **[${issue.secondLink}](url):** ${issue.secondLinkDescription}
+  **Project Mission:** ${issue.mission}
+  `;
 
-    **Project Name:** ${issue.name}
-    **Project Budget:** ${issue.budget}
-    **Project Duration:** ${issue.duration}
-    **Funding Stream:** ${selectedValues[0]}
-    **Project Type:** ${selectedValues[1]}
-    **Project Track:** ${selectedValues[2]}
-    **Project Goal:** ${selectedValues[3]}
-    **Project Audience:** ${selectedValues[4]}
-    **Project Openness:** ${selectedValues[5]}
+  console.log("template ", template);
 
-
-    ## This line will be converted into an H2 tag.
-
-    _This is an example of a line of text rendered in italics_
-
-    [This is text to be rendered into a link](url)
-
-    `;
-
-	console.log('template ', template);
-
-	return template;
+  return template;
 };
 
 export default IssueTemplate;
