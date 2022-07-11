c93571ae7f1b4747288a9905b7e6e3b5470b5f9e
Diopitis <54492027+diopitis@users.noreply.github.com>
2022-07-11 17:04:31 -0500

    added date picker and calculating last 7 days

--- pages/stacks-payment-converter.js
+++ pages/stacks-payment-converter.js
@@ -1,8 +1,20 @@
 import styles from './StacksPayment.module.css';
 import CloseIcon from '../public/images/close.svg';
 import StacksLogo from '../public/images/stacks-logo.svg';
+import DropdownIcon from '../public/images/dropdown.svg';
+import Calendar from 'react-calendar';
+import { useState } from 'react';
 
 const StacksConverter = () => {
+	const [value, onChange] = useState(new Date());
+	const [show, setShow] = useState(false);
+	// let pastSevenDays = value.getDate() - 7;
+	let pastSevenDays = new Date(new Date().setDate(value.getDate() - 7));
+	const copyAmount = () => {
+		let copyText = document.getElementById('stxNumber').innerText;
+		console.log(copyText);
+		navigator.clipboard.writeText(copyText);
+	};
 	return (
 		<div>
 			<div className={styles.close}>
@@ -38,13 +50,24 @@ const StacksConverter = () => {
 							onWheel={(e) => e.target.blur()}
 						/>
 					</div>
-					<div className={styles.dropdownWrapper}>
-						<label for="selectDate">B. Select Date Payment Issued</label>
-						<select name="selectDate">
-							<option value="" disabled selected>
-								Drop down...
-							</option>
-						</select>
+					<div className={styles.calendarDropdownWrapper}>
+						<h2 for="selectDate">B. Select Date Payment Issued</h2>
+						{console.log(show)}
+						<button
+							onClick={() => {
+								setShow(!show);
+							}}
+						>
+							<p>Drop Down...</p>
+							<DropdownIcon />
+						</button>
+						<div>
+							<Calendar
+								className={show ? styles.show : styles.hide}
+								onChange={onChange}
+								value={value}
+							/>
+						</div>
 					</div>
 					<div className={styles.dropdownWrapper}>
 						<button className={styles.converterButton}>Click to Convert</button>
@@ -52,20 +75,42 @@ const StacksConverter = () => {
 					<div className={styles.dropdownWrapper}>
 						<div>
 							<p>Date Range</p>
-							<p>Aug-24-22 to Sept-01-22 </p>
+							<p>
+								{pastSevenDays
+									.toLocaleString('default', {
+										month: 'long',
+										day: '2-digit',
+										year: '2-digit'
+									})
+									.replace(' ', '-')
+									.replace(',', '-')
+									.replace(' ', '')}
+								{` to `}
+								{value
+									.toLocaleString('default', {
+										month: 'long',
+										day: '2-digit',
+										year: '2-digit'
+									})
+									.replace(' ', '-')
+									.replace(',', '-')
+									.replace(' ', '')}
+							</p>
 							<p>in 7-day trailing average</p>
 						</div>
 					</div>
 					<div className={styles.dropdownWrapper}>
 						<div className={styles.stxAmount}>
 							<p>STX</p>
-							<p>768.39</p>
+							<p id="stxNumber">768.40</p>
 							<p>1 STX = 0.4 USD</p>
 						</div>
 					</div>
 					<div className={styles.dropdownWrapper}></div>
 					<div className={styles.dropdownWrapper}>
-						<button className={styles.clipboardButton}>Copy to clipboard</button>
+						<button className={styles.clipboardButton} onClick={copyAmount}>
+							Copy to clipboard
+						</button>
 					</div>
 				</div>
 			</div>
