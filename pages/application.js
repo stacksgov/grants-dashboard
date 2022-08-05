9e34a48b0534f8b71758c4e6fb2e043592831410
Diopitis <54492027+diopitis@users.noreply.github.com>
2022-08-05 13:16:25 -0500

    mixing quotation

--- pages/application.js
+++ pages/application.js
@@ -127,7 +127,7 @@ const Application = () => {
 		if (flow == 'B') {
 			// create GH discussion
 			const query = `mutation {
-        createDiscussion(input: {repositoryId: MDEwOlJlcG9zaXRvcnkyOTIwMDk4MzE=", categoryId: "MDE4OkRpc2N1c3Npb25DYXRlZ29yeTI1NzYzMDg1", body: "${markdown}", title: "Wishlist Idea"}) {
+        createDiscussion(input: {repositoryId: "MDEwOlJlcG9zaXRvcnkyOTIwMDk4MzE=", categoryId: "MDE4OkRpc2N1c3Npb25DYXRlZ29yeTI1NzYzMDg1", body: "${markdown}", title: "Wishlist Idea"}) {
           discussion {
             id
             url
