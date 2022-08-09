baaa715d7451f8838fbdc87c87b0755632051e58
kj <komacine@protonmail.com>
2022-08-09 12:52:22 +0100

    remove required email

--- components/application/ApplicantInformationB.js
+++ components/application/ApplicantInformationB.js
@@ -19,11 +19,7 @@ const ApplicantInformationB = () => {
           image={<GithubIcon />}
           readonly={true}
         />
-        <Input
-          name="email"
-          label="Email"
-          hint="(required, will be displayed on Github)"
-        />
+        <Input name="email" label="Email" />
       </Container>
       <Container>
         <Input
