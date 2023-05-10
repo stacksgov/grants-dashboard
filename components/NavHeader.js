a522e842ddd64ecd4076462a6a02e4ec7cdcdb36
Cornelius Canton <corneliuscantonii@gmail.com>
2023-05-10 14:34:43 -0400

    bounty page added as home page

---
+++ components/NavHeader.js
@@ -0,0 +1,22 @@
+import React,{useState} from 'react';
+import styles from './NavHeader.module.css';
+import Logo from '../public/images/StackFoundationLogo.svg'
+
+export default function NavHeader() {
+    const [mobNavActive,setMobNavActive] = useState(false);
+  return (
+    <div className={styles.navWrapper}>  
+        <Logo/>
+        <ul className={`${styles.navList} ${mobNavActive ? styles.active : ''}`}>
+            {/* <li onClick={()=>setMobNavActive(!mobNavActive)} className={styles.navListItem}>Apply for Grant</li>
+            <li onClick={()=>setMobNavActive(!mobNavActive)} className={styles.navListItem}>Manage Grant</li> */}
+            {/* <li onClick={()=>setMobNavActive(!mobNavActive)} className={styles.navListItem}>Grants Repo</li> */}
+        </ul>
+        {/* <div className={styles.mobNav} onClick={()=>setMobNavActive(!mobNavActive)}>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+        </div> */}
+    </div>
+  )
+}
